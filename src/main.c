4c9386e4b7105bd32ec78cb12c49e232acec0536
Ismail Debbarh <usamdh07@gmail.com>
2026-05-29 21:15:20 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,22 +6,37 @@ int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
 
 
-    char input[100];
 
     while(1){
+        char input[100];
+        char* parts[100] = {0};
+        size_t pc = 0;
+        char* token;
+
         printf("$ ");
 
         fgets(input, 100, stdin);
-
         input[strlen(input) - 1] = '\0';
 
-        if(strcmp(input, "exit") == 0){
+        token = strtok(input, " ");
+        while(token != NULL && pc < sizeof(parts) / sizeof(parts[0])){
+            parts[pc++] = token;
+            token = strtok(NULL, " ");
+        }
+
+        if(pc == 1 && strcmp(parts[0], "exit") == 0){
             break;
+        }else if(strcmp(parts[0], "echo") == 0){
+            size_t i = 1;
+
+            while(i < pc && parts[i] != NULL){
+                printf("%s ", parts[i++]);
+            }
+            printf("\n");
         }else{
             printf("%s: command not found\n", input);
         }
     }
     
-
-  return 0;
+    return 0;
 }
