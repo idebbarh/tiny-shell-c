05f48b9fcf4fbc5e72579e1dd15138b29e4e2d96
Ismail Debbarh <ismaildebbarh1@gmail.com>
2026-05-29 20:28:19 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  char input[50];
-
   printf("$ ");
-  scanf("%s", input);
-  printf("%s: command not found\n", input);
 
+  char input[100];
+  fgets(input, 100, stdin);
+
+  input[strlen(input) - 1] = '\0';
+  printf("%s: command not found\n", input);
   return 0;
 }
