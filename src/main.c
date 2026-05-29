e3c3f765e30751bc90a5ace70612e885d837e080
Ismail Debbarh <ismaildebbarh1@gmail.com>
2026-05-29 20:22:39 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -5,7 +5,11 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
+  char input[50];
+
   printf("$ ");
+  scanf("%s\n", input);
+  printf("{%s}: command not found\n", input);
 
   return 0;
 }
