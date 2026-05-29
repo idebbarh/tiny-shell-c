246bb6732072eb60d99f49cf2dbe6123c467db60
Ismail Debbarh <usamdh07@gmail.com>
2026-05-29 21:32:01 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -18,23 +18,41 @@ int main(int argc, char *argv[]) {
         fgets(input, 100, stdin);
         input[strlen(input) - 1] = '\0';
 
-        token = strtok(input, " ");
-        while(token != NULL && pc < sizeof(parts) / sizeof(parts[0])){
-            parts[pc++] = token;
-            token = strtok(NULL, " ");
-        }
+        if(strlen(input) > 0){
 
-        if(strcmp(input, "exit") == 0){
-            break;
-        }else if(strcmp(parts[0], "echo") == 0){
-            size_t i = 1;
-
-            while(i < pc){
-                printf("%s ", parts[i++]);
+            token = strtok(input, " ");
+            while(token != NULL && pc < sizeof(parts) / sizeof(parts[0])){
+                parts[pc++] = token;
+                token = strtok(NULL, " ");
             }
-            printf("\n");
-        }else{
-            printf("%s: command not found\n", input);
+
+            if(strcmp(input, "exit") == 0){
+                break;
+            }else if(strcmp(parts[0], "echo") == 0){
+                size_t i = 1;
+
+                while(i < pc){
+                    printf("%s ", parts[i++]);
+                }
+
+                if(i > 1){
+                    printf("\n");
+                }
+            }else if(strcmp(parts[0], "type") == 0){
+                size_t i = 1;
+
+                while(i < pc){
+                    char* cmd = parts[i++];
+
+                    if(strcmp(cmd, "echo") == 0 || strcmp(cmd, "exit") == 0 || strcmp(cmd, "type") == 0){
+                        printf("%s is a shell builtin\n", cmd);
+                    }else {
+                        printf("%s: not found\n", cmd);
+                    }
+                }
+            }else{
+                printf("%s: command not found\n", input);
+            }
         }
     }
     
