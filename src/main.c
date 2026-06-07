e26aa82c15714281a8912340ec1ff8674fb2893f
Ismail Debbarh <usamdh07@gmail.com>
2026-06-07 01:23:09 +0100

    feat: Displaying registered specifications [skip ci]

--- src/main.c
+++ src/main.c
@@ -217,7 +217,7 @@ int lookup_program(char *cmd, char full_path[PATH_MAX]) {
 
 char *cmd_name_generator(const char *text, int state) {
   static int list_index, len, is_external;
-  struct dirent *entry;
+  static struct dirent *entry;
   static char *name, *path, *subpath;
   static DIR *dir;
   char *result = NULL;
