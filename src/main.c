86a0dd8fc9e569bc09a1158f20445d8107292b4b
Ismail Debbarh <usamdh07@gmail.com@gmail.com>
2026-06-12 16:48:59 +0100

    feat: Multiple completer candidates [skip ci]

--- src/main.c
+++ src/main.c
@@ -419,12 +419,12 @@ char **cmd_name_completion(const char *text, int start, int end) {
   if (find_complete_history_completer(complete_cmd_state.complete_history,
                                       complete_cmd_state.complete_history_size,
                                       first_arg, &completer)) {
-    snprintf(
-        completer_with_args, INPUT_CAPACITY,
-        "bash -c 'COMP_LINE=\"%s\" COMP_POINT=%ld strace %s %s %s %s' 2>&1",
-        rl_line_buffer, strlen(rl_line_buffer), completer,
-        first_arg == NULL ? "" : first_arg, text,
-        third_arg == NULL ? "" : second_arg);
+
+    snprintf(completer_with_args, INPUT_CAPACITY,
+             "bash -c \"COMP_LINE='%s' COMP_POINT=%ld %s %s %s %s\"",
+             rl_line_buffer, strlen(rl_line_buffer), completer,
+             first_arg == NULL ? "" : first_arg, text,
+             third_arg == NULL ? "" : second_arg);
 
     FILE *completer_stdout = popen(completer_with_args, "r");
 
@@ -435,11 +435,9 @@ char **cmd_name_completion(const char *text, int start, int end) {
 
     if (completer_stdout != NULL) {
       while ((ch = fgetc(completer_stdout)) != EOF) {
-        printf("The char: %c\n", ch);
         if (ch == '\n')
           line_count++;
       }
-      printf("The lines count: %ld\n", line_count);
 
       pclose(completer_stdout);
     }
@@ -451,8 +449,6 @@ char **cmd_name_completion(const char *text, int start, int end) {
 
       while (fgets(line, sizeof(line), completer_stdout) != NULL &&
              index < line_count) {
-        printf("The line: %s\n", line);
-
         size_t len = strlen(line);
 
         if (len > 0 && line[len - 1] == '\n') {
