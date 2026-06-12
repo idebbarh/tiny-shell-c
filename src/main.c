3f9f40be5ee8b3f72659c27ad6418ba5e5e7f272
Ismail Debbarh <ismaildebbarh1@gmail.com>
2026-06-12 12:51:42 +0100

    feat: Multiple completer candidates [skip ci]

--- src/main.c
+++ src/main.c
@@ -420,11 +420,15 @@ char **cmd_name_completion(const char *text, int start, int end) {
                                       complete_cmd_state.complete_history_size,
                                       first_arg, &completer)) {
 
+    printf("\nThe completer found: %s\n", completer);
+
     snprintf(completer_with_args, INPUT_CAPACITY,
              "COMP_LINE='%s' COMP_POINT=%ld %s %s %s %s", rl_line_buffer,
              strlen(rl_line_buffer), completer,
              first_arg == NULL ? "" : first_arg, text,
              third_arg == NULL ? "" : second_arg);
+
+    printf("\nThe completer with args found: %s\n", completer_with_args);
 
     FILE *completer_stdout = popen(completer_with_args, "r");
 
@@ -435,11 +439,12 @@ char **cmd_name_completion(const char *text, int start, int end) {
       int ch;
 
       while ((ch = fgetc(completer_stdout)) != EOF) {
-
-        printf("current char: %c\n", ch);
         if (ch == '\0')
           line_count++;
       }
+
+      printf("\nThe completer lines: %ld\n", line_count);
+
       rewind(completer_stdout);
 
       curr_completer_value = calloc(line_count + 1, sizeof(char *));
